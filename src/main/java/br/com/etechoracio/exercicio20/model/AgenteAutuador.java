3c3872a0acf292de217c2c30ba989554ce00133f
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-05-08 22:06:39 -0300

    Ex 20 - Enumeração

---
+++ src/main/java/br/com/etechoracio/exercicio20/model/AgenteAutuador.java
@@ -0,0 +1,41 @@
+package br.com.etechoracio.exercicio20.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.etechoracio.exercicio20.enums.StatusAgenteEnum;
+import br.com.etechoracio.exercicio20.enums.TipoAgenteEnum;
+
+@Entity
+@Table (name="TBL_AGENTE_AUTUADOR")
+public class AgenteAutuador { 
+
+	@Id
+	@GeneratedValue
+	@Column(name="ID_AGENTE")
+	private long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="TP_AGENTE")
+	private TipoAgenteEnum tipo;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="ST_AGENTE")
+	private StatusAgenteEnum Status;
+	
+	@Column(name="TX_NOME")
+	private String nome;
+	
+	@Column(name="TX_FONE")
+	private String telefone;
+	
+	
+	
+	
+	
+}
